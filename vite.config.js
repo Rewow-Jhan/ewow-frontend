8ff8f352d9aed76fcfc3474dc2115ad6bd877588
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-03-28 17:16:24 -0500

    Feat: Tailwinf and daisyUi imports

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
@@ -14,5 +16,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    }
   },
 })
