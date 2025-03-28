9e63864513f7469790eca4094359fbd3a7358db4
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-03-28 17:25:50 -0500

    Feat: Config vue-axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import * as Vue from 'vue' // in Vue 3
+
+import axiosPlugin from './plugins/vue-axios'
+
+const app = Vue.createApp(App)
+
 
 app.use(createPinia())
 app.use(router)
+app.use(axiosPlugin, {})
 
 app.mount('#app')
